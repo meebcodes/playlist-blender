Added conic gradient, fixed template bug

gradient_generator/gradient_generator.py
@@ -2,7 +2,7 @@ import numpy as np
 from PIL import Image
 import colorsys
 import io
-from math import sqrt
+from math import sqrt, atan2, pi
 
 '''
 We will be generating 300x300 PNG images in RGB.
@@ -297,6 +297,41 @@ def radial_alg_hsv(c1, c2, radius, center, x, y):
         v = ((c2[2] - c1[2]) * ((radius - dist_from_center) / radius) + c1[2])
         return (h, s, v)
 
+def gen_linear_conic_grad_hsv_interp(c1, c2) -> io.BytesIO():
+    '''
+    Generate an image with a two-color linear conic gradient using HSV values.
+
+    Returns PNG data in BytesIO object.
+    '''
+
+    # generate starting array
+    a = np.zeros((HEIGHT, WIDTH, RGB_VAL), dtype='uint8')
+
+    # interpolate between the two colors
+    for y in range(HEIGHT):
+        for x in range(WIDTH):
+            h, s, v = conic_alg_hsv(c1, c2, (x - 150), (y - 150))
+            a[y][x][0], a[y][x][1], a[y][x][2] = tuple(map(lambda x: int(x * 255), list(colorsys.hsv_to_rgb(h, s, v))))
+    
+    # make an image from the array and save it as a PNG
+    img = Image.fromarray(a)
+    img_bytes = io.BytesIO()
+    img.save(img_bytes, format="PNG")
+    return img_bytes
+
+def conic_alg_hsv(c1, c2, x, y):
+
+    degree = atan2(y, x)
+    degree += pi
+
+    percentage = degree / (2 * pi)
+
+    h = ((c2[0] - c1[0]) * percentage + c1[0])
+    s = ((c2[1] - c1[1]) * percentage + c1[1])
+    v = ((c2[2] - c1[2]) * percentage + c1[2])
+
+    return (h, s, v)
+
 # ============
 #   EXTERNAL
 # ============
@@ -377,6 +412,25 @@ def gen_linear_diamond_grad_from_attr(tempo, valence, energy, acousticness) -> i
     c2 = [c1[0] + .2 * range, c1[1], c1[2]]
     return gen_linear_diamond_grad_hsv_interp(c1, c2)
 
+def gen_linear_conic_grad_from_attr(tempo, valence, energy, acousticness) -> io.BytesIO():
+    '''
+    Generate a linear diamond-shaped gradient image from song attributes.
+
+    tempo: determines range of hues in gradient
+    valence: determines starting hue, value
+    energy: determines saturation (additive)
+    acousticness: determines saturation (subtractive)
+
+    Returns PNG data in BytesIO object.
+    '''
+    generator_inputs = attr_to_gen_input(tempo, valence, energy, acousticness)
+    hsv = generator_inputs[0]
+    range = generator_inputs[1]
+    
+    c1 = hsv
+    c2 = [c1[0] + .2 * range, c1[1], c1[2]]
+    return gen_linear_conic_grad_hsv_interp(c1, c2)
+
 # ============
 #   Testing
 # ============
@@ -386,36 +440,37 @@ if __name__ == '__main__':
     # These are hardcoded values just used for testing the appearance of gradients.
 
     # asobi seksu: strawberries
-    img_data = gen_linear_vert_grad_from_attr(tempo=130.542, valence=0.350, energy=0.859, acousticness=0.000322)
+    img_data = gen_linear_conic_grad_from_attr(tempo=130.542, valence=0.350, energy=0.859, acousticness=0.000322)
     img = Image.open(fp=img_data)
     img.save(fp="strawberries.png")
 
+
     # caroline polachek: welcome to my island
-    img_data = gen_linear_horiz_grad_from_attr(tempo=118.000, valence=0.360, energy=0.627, acousticness=0.0836)
+    img_data = gen_linear_conic_grad_from_attr(tempo=118.000, valence=0.360, energy=0.627, acousticness=0.0836)
     img = Image.open(fp=img_data)
     img.save(fp="welcome.png")
 
     # tennis: glorietta
-    img_data = gen_linear_horiz_grad_from_attr(tempo=169.034, valence=0.799, energy=0.839, acousticness=0.0183)
+    img_data = gen_linear_conic_grad_from_attr(tempo=169.034, valence=0.799, energy=0.839, acousticness=0.0183)
     img = Image.open(fp=img_data)
     img.save(fp="glorietta.png")
 
     # flying saucer attack: my dreaming hill
-    img_data = gen_linear_horiz_grad_from_attr(tempo=128.090, valence=0.197, energy=0.696, acousticness=0.233)
+    img_data = gen_linear_conic_grad_from_attr(tempo=128.090, valence=0.197, energy=0.696, acousticness=0.233)
     img = Image.open(fp=img_data)
     img.save(fp="dreaming.png")
 
     # tv priest: bury me in my shoes
-    img_data = gen_linear_horiz_grad_from_attr(tempo=134.011, valence=0.0712, energy=0.663, acousticness=0.000184)
+    img_data = gen_linear_conic_grad_from_attr(tempo=134.011, valence=0.0712, energy=0.663, acousticness=0.000184)
     img = Image.open(fp=img_data)
     img.save(fp="bury.png")
 
     # measuring windows playlist
-    img_data = gen_linear_horiz_grad_from_attr(tempo=115.35723333333331, valence=0.35263999999999995, energy=0.6138333333333332, acousticness=0.32758713)
+    img_data = gen_linear_conic_grad_from_attr(tempo=115.35723333333331, valence=0.35263999999999995, energy=0.6138333333333332, acousticness=0.32758713)
     img = Image.open(fp=img_data)
     img.save(fp="windows.png")
 
     # let me play guitar in your math rock band playlist
-    img_data = gen_linear_horiz_grad_from_attr(tempo=123.25553571428567, valence=0.4888749999999999, energy=0.7668392857142857, acousticness=0.08682526839285713)
+    img_data = gen_linear_conic_grad_from_attr(tempo=123.25553571428567, valence=0.4888749999999999, energy=0.7668392857142857, acousticness=0.08682526839285713)
     img = Image.open(fp=img_data)
     img.save(fp="math.png")